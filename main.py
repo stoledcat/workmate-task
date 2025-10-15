62f177442ab9688e8a7a70f75215de98ef28e767
stoledcat <jetnavo@yandex.ru>
2025-10-15 23:17:30 +0300

    Добавлены основные аргументы

--- main.py
+++ main.py
@@ -1,2 +1,22 @@
 import argparse
+# import csv
+# import tabulate
 
+
+def make_parser():
+    parser = argparse.ArgumentParser(
+        prog="BrandRatingAnalysis", description="Анализ рейтинга брендов"
+    )
+    
+    parser.add_argument("--files", nargs="+")  # "+" 1 и больше аргументов
+    parser.add_argument("--report")
+    list_csv = parser.parse_args().files
+    parse_csv_files(list_csv)
+
+
+def parse_csv_files(list_csv):
+    pass
+
+
+if __name__ == "__main__":
+    make_parser()
