233a750f740601c3dbfe4c2a8389aeb7fb57c394
stoledcat <jetnavo@yandex.ru>
2025-10-16 13:52:08 +0300

    Реализовано формирование словаря с рассчитанным рейтингом брендов

--- main.py
+++ main.py
@@ -1,22 +1,55 @@
 import argparse
-# import csv
+import csv
 # import tabulate
 
 
-def make_parser():
+def main():
     parser = argparse.ArgumentParser(
         prog="BrandRatingAnalysis", description="Анализ рейтинга брендов"
     )
-    
+
     parser.add_argument("--files", nargs="+")  # "+" 1 и больше аргументов
-    parser.add_argument("--report")
+    parser.add_argument("--report", choices=["average-rating"])
+    args = parser.parse_args()
+
+    if args.report == "average-rating":
+        brands_dict = average_rating(parser)
+        print(brands_dict)
+
+
+def average_rating(parser):
     list_csv = parser.parse_args().files
-    parse_csv_files(list_csv)
+    return parse_csv_files(list_csv)
 
 
-def parse_csv_files(list_csv):
-    pass
+def parse_csv_files(list_csv: list) -> dict:
+    brands_dict = {}
+
+    # list_csv = ["products1.csv", "products2.csv"]
+
+    for file in list_csv:
+        with open(file, newline="") as csvfile:
+            filereader = csv.reader(csvfile)
+            next(filereader)  # Пропустить шапку документа (name brand price rating)
+
+            for row in filereader:
+                brand = row[1]
+                rating = float(row[3])
+
+                if brand in brands_dict:
+                    brands_dict[brand][0] += rating
+                    brands_dict[brand][1] += 1
+                else:
+                    # Записать новый ключ (бренд: рейтинг, счётчик)
+                    brands_dict[brand] = [rating, 1]
+
+    for brand in brands_dict:
+        brand_total_rating = brands_dict.get(brand)[0]
+        brand_repeat_counters = brands_dict.get(brand)[1]
+        brands_dict[brand] = round(brand_total_rating / brand_repeat_counters, 2)
+
+    return brands_dict
 
 
 if __name__ == "__main__":
-    make_parser()
+    main()
