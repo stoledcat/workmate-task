597cbda26af218725e5b59d62d913c9b95313894
stoledcat <jetnavo@yandex.ru>
2025-10-17 13:24:39 +0300

    Добавлен тест на проверку сортировки таблицы с результатами расчетов

--- tests/test_main.py
+++ tests/test_main.py
@@ -16,4 +16,8 @@ def test_check_csv_files():
 
 
 def test_sort_brands_list():
-    pass
+    brands_dict = {"apple": [11.20, 4], "samsung": [13.6, 3], "xiaomi": [13.1, 3]}
+    sorted_brands_list = main.sort_brands_list(brands_dict)
+    for i in range(len(sorted_brands_list) - 1):
+        assert sorted_brands_list[i][1] > sorted_brands_list[i + 1][1]
+
