822948c544f868c6e9bfc3e1d4949a8a3aa8c2a9
stoledcat <jetnavo@yandex.ru>
2025-10-17 12:43:54 +0300

    Тесты парсинга переданных аргументов и сортировки результатов

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,15 +1,19 @@
 import main
 
 
+def test_main_parser():
+    parser = main.main_parser(
+        ["--files", "products1.csv", "products2.csv", "--report", "average-rating"]
+    )
+    assert parser.files == ["products1.csv", "products2.csv"]
+    assert parser.report == "average-rating"
+
+
 def test_check_csv_files():
     list_csv = ["products1", "products2"]
     output_list_csv = main.check_csv_files(list_csv)
     assert output_list_csv == ["products1.csv", "products2.csv"]
 
 
-def test_main():
-    pass
-
-
-def test_make_brands_dict():
+def test_sort_brands_list():
     pass
